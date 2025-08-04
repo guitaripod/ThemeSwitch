b9ef9b042757266cab5f207817578bea021b0403
guitaripod <guitaripod@icloud.com>
2025-08-05 01:00:53 +0300

    feat: add resource files for dialog and icons

---
+++ resource.h
@@ -0,0 +1,14 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+#define IDI_LIGHT                       101
+#define IDI_DARK                        102
+#define IDD_SETTINGS                    103
+#define IDC_CHECK_STARTUP               1001
+#define IDC_CHECK_ICON                  1002
+#define IDC_HOTKEY_DISPLAY              1003
+#define IDC_BUTTON_CHANGE_HOTKEY        1004
+#define IDC_THEME_STATUS                1005
+#define IDC_VERSION                     1006
+
+#endif
